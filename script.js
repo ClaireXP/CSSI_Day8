5a8063cee0cb2f74574aa7065aed6a517763bb26
Glitch (hello-webpage) <none>
2020-07-15 17:26:14 +0000

    🕶👝 Checkpoint ./script.js:18576123/1121

--- script.js
+++ script.js
@@ -37,36 +37,31 @@ let yCan = window.innerHeight-20;
 let drop1x, drop1y, drop1d, drop1FallSpeed;
 
 let drops = [];
+let maxFallSpeed = 8;
+let numDrops = 25;
 
 function setup() {
   createCanvas(xCan, yCan);
   colorMode(HSB, 100);
-  // Variables for droplet 1
-  drop1x = 200; // or random(width)
-  drop1y = 0; // or random(height)
-  drop1d = 10; // or random(5,15)
-  drop1FallSpeed = 8; // or random(8, 20)
-
-  // Variables for droplet 2
+  
+  
 }
 
 function draw() {
   background(0, 0, 95);
-  //// Code for droplet 1
-  // Move droplet 1
-  drop1y += drop1FallSpeed;
-  // If it goes off the screen...
-  if (drop1y > height) {
-    // ...reset it...
-    drop1y = 0;
-    // ...and move it somewhere random.
-    drop1x = random(width);
-  }
-  // Display droplet 1
-  noStroke();
-  fill(60, 80, 80);
-  ellipse(drop1x, drop1y, drop1d);
+}
 
-  //// Code for droplet 2
-  // Code your next droplet here
+class drop{
+  constructor(x, y, r){
+    this.x = random(r, xCan-r);
+    this.y = -r;
+    this.r = r;
+    this.speed = random(maxFallSpeed);
+  }
+  
+  show(){
+    noStroke();
+    fill(60, 80, 80);
+    ellipse(this.x, this.y, this.r);
+  }
 }
